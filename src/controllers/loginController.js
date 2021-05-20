124713da09db7b6e2d37a4e07d9bb5cc3600ccbf
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2021-05-20 17:07:20 -0400

    Adição de validações.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -8,6 +8,20 @@ module.exports = {
         const url = "https://auth.leadszapp.com/api/login";
         const Authorization = "S_B0arEKEd0mK3ymyeYc2w3U73z1VxmmALH_CNeWAKU";
 
+        req.assert("loginId", "0").notEmpty();
+        req.assert("loginId", "1").isEmail();
+        req.assert("password", "2").notEmpty();
+        let errors = req.validationErrors();
+        let validate = [];
+
+        for (let i = 0; i < errors.length; i++) {
+            validate[errors[i].msg] = errors[i].msg;
+        }
+        if (errors) {
+            res.json(validate);
+            return;
+        }
+
         axios.post(url, {
             loginId,
             password
@@ -18,10 +32,12 @@ module.exports = {
             }
         })
         .then(response => {
-            res.status(200).json(response.data.token).end();
+            if (response.data.token) {
+                res.status(200).json("Successful");
+            }
         })
         .catch(err => {
-            console.log(err);
+            res.status(400).json("3");
         });
 
     }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const expressValidator = require("express-validator");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(expressValidator());
 app.use(routes);
 
 app.listen(3333, () => console.log("Server On"));
